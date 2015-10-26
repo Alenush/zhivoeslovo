34c52768be5de79c9dccbb4631040dc4d7f6f657
alenush <alenusch93@yandex.ru>
2015-10-27 01:10:01 +0300

    CHANGED MARK UP. FIND BUG with pairs. Changed readme. Clean Db. Add a bit template.

--- zh_slovo/views.py
+++ zh_slovo/views.py
@@ -6,26 +6,32 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from .models import Dict_text, Errors_table, Answer_user
 import difflib, re, random, simplejson
+import math
 
 
-def check_errors_in_db(result):
+def check_errors_in_db(result): #add id_dict and text_user
     """
     Find, where !=equal
     Function takes from diff resuls (array with ("equal, a1,a2, b1,b2") )
     :param result:
     :return: array from (error-object, tok_begin for error tok_end)
+    NOTE: ? если у пользователя , или др. пунктуация, то
+    b1-b2 кусок беру - это \запятая\, проверяю, что это пунктуация и не равно тире. ?
+    Добавляем ошибку и пишем - ЗДЕСЬ ОШИБКА ПУНКТАЦИОННАЯ
     """
     dictation = Dict_text.objects.get(pk=1)
     origin, dic_id = dictation.dic_origin_text, dictation.id
     errors = []
     for op, a1, a2, b1, b2 in result:
         if op != "equal":
-            er_object = Errors_table.objects.filter(id_dict=dic_id,symbol_place_in_sent=a1)
-            if len(er_object) != 0:
-                object_dic = er_object.values()[0]
-                tok_begin, tok_end = object_dic["token_border_begin"], object_dic["token_border_end"]
-                errors.append((er_object, tok_begin, tok_end))
-    #if more than ? return СМ,
+            if a2 == a1: # при вставке мы хотим иметь непустой диапазон букв со стороны orig
+                a2 += 1
+            for pos in range(a1, a2):
+                er_object = Errors_table.objects.filter(id_dict=dic_id,symbol_place_in_sent=pos)
+                if len(er_object) != 0:
+                    object_dic = er_object.values()[0]
+                    tok_begin, tok_end = object_dic["token_border_begin"], object_dic["token_border_end"]
+                    errors.append((er_object, tok_begin, tok_end))
     return errors
 
 
@@ -34,9 +40,18 @@ def orig_to_user(result):
     # для диапазонов в user берём крайнюю левую координату
     #переводит координату одного символа из ориг в юсер
     orig2user = {}
-    for tup in result:
-        for i, j in zip(range(tup[1], tup[2]+1), range(tup[3], tup[4]+1)):
+    for operation, a1, a2, b1, b2 in result:
+        a2, b2 = a2 + 1, b2 + 1 # грязный хак: хотя difflib выдаёт диапазоны уже пригодными
+        # для range, мы добавляем в каждый из них по пустой букве, чтобы каждый край delete
+        # и insert получил по соответствующему символу в пару;
+        # это не должно испортить отображение ощутимо, т.к. лишняя буква будет переписана более
+        # точным соответствием на следующей итерации цикла
+        stretch = int(math.ceil(float(a2 - a1)/(b2 - b1)))
+        user_range = [pos for pos in range(b1, b2) for repeat in xrange(stretch)]
+        print operation, a1, a2, b1, b2, "stretch", stretch, "user", user_range
+        for i, j in zip(range(a1, a2), user_range):
             orig2user[i] = j
+    print orig2user
     return orig2user
 
 
@@ -47,26 +62,32 @@ def token_borders2user(origin2user, errors):
     user_er_tokens = []
     for error in errors:
         user_er_tokens.append((origin2user[error[1]], origin2user[error[2]]))
+    print user_er_tokens
     return user_er_tokens
 
 
-def fill_user_arrays(user_borders, errors):
+def fill_user_arrays(user_borders, errors, dict_text):
     """
     :param:user_borders: [token begin, token end]
     :param:errors: array with (er_object, tok_begin, tok_end))
     заводим список из кучи []; проходим по найденным диапазонам в координатах user,
     и дописываем в каждый [] по этим координатам описания ошибок, соответствующие этому диапазону
+    BUG! two errors in one or empty "" text
     :return:
     """
-    array = [[]]*50
+    array = [[] for n in xrange(len(dict_text))]
+    print "first array", array
     for i in range(0, len(array)):
         all_errors = []
-        for teil in user_borders:
-            er = errors[user_borders.index(teil)][0]
-            all_errors.append(er)
-            if i in range(teil[0],teil[1]+1):
+        for part in user_borders:
+            er = errors[user_borders.index(part)][0]
+            if i in range(part[0],part[1]+1):
+                all_errors.append(er)
                 array[i] = all_errors
+            print "all_errors", all_errors
+    print array
     return array
+
 
 def error2json(error):
     """Convert error object to dictionary suitable for JSON.
@@ -82,18 +103,27 @@ def collect_markup(parts_array, user_text):
     """- проходим по zip(от этого списка и текста user), и собираем словари { text, errors }
     из каждой цепочки идущих подряд букв, имеющих одинаковые ошибки
     """
-    markup = {}
+    markup = []
     text_part = ""
     last_error = []
-    error_info = []
+    dictionary = {}
+    print "Array_wth_parts", parts_array
     for user, error in zip(user_text, parts_array):
         if error == last_error:
             text_part += user
         else:
-            markup[text_part] = map(error2json, last_error)
+            answer = map(error2json, last_error)
+            dictionary["text"] = text_part
+            dictionary["errors"] = answer
+            markup.append(dictionary)
             text_part = user
             last_error = error
-    markup[text_part] = map(error2json, last_error)
+            dictionary = {}
+    answer = map(error2json, last_error)
+    dictionary["text"] = text_part
+    dictionary["errors"] = answer
+    markup.append(dictionary)
+    print "MARK UP: ", markup
     return markup
 
 
@@ -102,11 +132,11 @@ def count_errors(markup):
     # сколько хотя бы одну пунктуационную; эти числа и называем числом ошибок
     OR = 0
     PU = 0
-    for key in markup.keys():
-        for ar in markup[key]:
-            if "OR" in ar.values():
+    for dict_string_part in markup:
+        for er_dic in dict_string_part["errors"]:
+            if "OR" in er_dic.values():
                 OR +=1
-            elif "PU" in ar.values():
+            elif "PU" in er_dic.values():
                 PU += 1
     return OR, PU
 
@@ -119,11 +149,15 @@ def diff_strings(user, origin):
     :return: sentence with + -. - delete, + add right
     """
     dif = difflib.SequenceMatcher(unicode.isspace, origin, user)
+    print u'\n'.join(u"{}: {}~{} {} / {}~{} {}".format(op, a1, a2, origin[a1:a2], b1, b2, user[b1:b2]) for op, a1, a2, b1, b2 in dif.get_opcodes())
     result = dif.get_opcodes()
     origin2user = orig_to_user(result) # dictionary of orig to user match
     errors = check_errors_in_db(result) # array with (er_object, tok_begin, tok_end))
+    print "errors", errors
     user_borders = token_borders2user(origin2user, errors) # user tokens with errors
-    array_with_parts = fill_user_arrays(user_borders, errors) #array with [[][][][Error][][Error][Error]]
+    print "borders", user_borders
+    array_with_parts = fill_user_arrays(user_borders, errors, user) #array with [[][][][Error][][Error][Error]]
+    print "parts", array_with_parts
     markup = collect_markup(array_with_parts, user)
     or_er, p_er = count_errors(markup)
     grade = return_user_grade(or_er, p_er)
@@ -146,7 +180,7 @@ def return_user_grade(orph,punct):
     elif (orph,punct) in three:
         return u"3"
     else:
-        return u"См."
+        return u"2"
 
 
 def add_hash_number():
@@ -155,6 +189,7 @@ def add_hash_number():
     :return:
     """
     hash = random.getrandbits(128)
+    print "%032x" % hash
     return "%032x"%hash
 
 
@@ -165,7 +200,8 @@ def begin_dict(request):
     dictation = Dict_text.objects.get(pk=1)
     link = dictation.video_link
     all_dict = Dict_text.objects.all()
-    return render(request,'write_dict.html', {"video":link, "dictations":all_dict})
+    print dictation, "Data_time"
+    return render(request,'write_dict.html', {"video":link, "dictation":dictation})
 
 
 def count_results(request):
