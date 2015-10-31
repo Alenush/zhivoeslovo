a57e8cc05fc2ed34d1e5861b941bded32f3b99d2
Danya Alexeyevsky <me.dendik@gmail.com>
2015-11-01 02:17:59 +0300

    Changed links to display announce by default

--- zh_slovo/urls.py
+++ zh_slovo/urls.py
@@ -6,10 +6,10 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^$', views.begin_dict, name='begin_dict'),
+    url(r'^begin/$', views.begin_dict, name='begin_dict'),
     url(r'^ajax/results/$', views.count_results, name='result_dict'),
     url(r'^ajax/send_results/$', views.send_good_result, name="send_result"),
-    url(r'test/', views.test, name='test_json'),
-    url(r'anons/', views.anons, name='anons'),
-    url(r'success/', views.success, name='success')
+    url(r'^test/', views.test, name='test_json'),
+    url(r'^$', views.anons, name='anons'),
+    url(r'^success/', views.success, name='success')
 ]
