394378c168f2cc9b213319ce363cc7975f244003
Danya Alexeyevsky <me.dendik@gmail.com>
2015-11-07 03:31:19 +0300

    Added import/export admin extension

--- zh_slovo/admin.py
+++ zh_slovo/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from .models import Dict_text, Answer_user, Errors_table
 
+class ErrorsResource(resources.ModelResource):
+    class Meta:
+        model = Errors_table
+
+class ErrorsAdmin(ImportExportModelAdmin):
+    resource_class = ErrorsResource
+    pass
 
 # Register your models here.
 admin.site.register(Dict_text)
-admin.site.register(Errors_table)
+admin.site.register(Errors_table, ErrorsAdmin)
 admin.site.register(Answer_user)
