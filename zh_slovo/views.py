e9130736483538fe88cc19f4d79fcbc71a5035c5
Danya Alexeyevsky <me.dendik@gmail.com>
2015-11-23 13:11:41 +0300

    Removed - replacement for dictation 13

--- zh_slovo/views.py
+++ zh_slovo/views.py
@@ -225,7 +225,6 @@ def normalize_user_text(user_text):
     #user_text = user_text.replace(' -',':') for 8 dikt it is bad
     user_text = user_text.replace(';',',')
     user_text = user_text.replace(':',',')
-    user_text = user_text.replace('-',',')
     #user_text = user_text.replace(u'ё',u'е')
     user_text = space_re.sub(' ', user_text)
     user_text = sentence_re.sub('.', user_text)
